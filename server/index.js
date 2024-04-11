2710c3e56cc8198b8ee8b9bb7c3213e39a7cefe2
agadfs <henriquedefreitasserra@hotmail.com>
2024-04-11 15:44:31 -0300

    v1

--- server/index.js
+++ server/index.js
@@ -281,6 +281,17 @@ app.post("/npcscreate", async (req, res) => {
   }
 });
 
+app.get("/npcsGET", async (req, res) => {
+  
+  try {
+    const sessions = await npcs.find();
+    res.status(200).json(sessions);
+  } catch (error) {
+    console.error("Error fetching sessions:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
